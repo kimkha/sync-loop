2c9a13c3199fc0c59115eb6c87283c0f0709b421
Nguyễn Kim Kha <nkimkha@gmail.com>
2017-04-27 10:51:39 +0700

    Update sync-loop

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/**
+ * Run sync loop with async function
+ *
+ * @example
+ * var syncLoop = require('sync-loop');
+ * var numberOfLoop = 10;
+ * syncLoop(numberOfLoop, function (loop) {
+ *  // loop body
+ *  var index = loop.iteration(); // index of loop, value from 0 to (numberOfLoop - 1)
+ *  doAsyncJob(function(){
+ *    // This is callback of your function
+ *    loop.next(); // call `loop.next()` for next iteration
+ *  })
+ *}, function () {
+ *  console.log("This is finish function")
+ *});
+ */
 module.exports = function (iterations, process, exit) {
   var index = 0,
     done = false,
